d7dc0e2f30568de1719a2e0d7da0f531f09d2afe
michael youssef <michaelyoussef396@gmail.com>
2024-06-15 11:26:43 +1000

    created top nav

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import TopNav from "@/components/TopNav";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <TopNav />
+    </main>
+  );
+}
---
+++ components/TopNav.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+
+const TopNav = () => {
+  return (
+    <div className="w-full bg-black text-white p-4 flex justify-between items-center pl-12">
+      <div className="flex items-center space-x-2">
+        <Image src="/mail.png" alt="mail" width={20} height={20} />
+        <a href="mailto:info@edeninternational.in" className="hover:underline">info@edeninternational.in</a>
+      </div>
+      <div className="flex items-center space-x-2 pr-12">
+        <Image src="/phone.png" alt="phone" width={20} height={20} />
+        <span>+61</span>
+      </div>
+    </div>
+  );
+};
+
+export default TopNav;
